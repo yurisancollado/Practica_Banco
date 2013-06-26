58f02bb9366bbd34a3c2282705ff0a53ef3b07cd
Yurisan Collado <yurisan29@hotmail.com>
2013-06-26 15:21:37 -0430

    Creando Proyecto
    Proyecto Java Netbeans

---
+++ src/practica_banco/Practica_Banco.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica_banco;
+
+/**
+ *
+ * @author Yuri
+ */
+public class Practica_Banco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
