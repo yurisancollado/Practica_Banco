34bf876ab8e8111c04a6b6e260ac908078168572
Carlos Ruiz <crtx000@gmail.com>
2013-06-27 23:15:43 -0430

    Revert "Revert "Cuenta Bancaria y Reportes""
    This reverts commit 6943cc9fbc33b0c4d88a0ee964371a1b85a48a4f.

--- src/practica_banco/Cliente.java
+++ src/practica_banco/Cliente.java
@@ -17,6 +17,49 @@ public class Cliente {
     private String cedulaCliente;
     private ArrayList<CuentaBancaria> listaCuentaBancaria = new ArrayList<CuentaBancaria>();
 
+    public String getCedulaCliente() {
+        return cedulaCliente;
+    }
+
+    public void setCedulaCliente(String cedulaCliente) {
+        this.cedulaCliente = cedulaCliente;
+    }
+
+    public long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public ArrayList<CuentaBancaria> getListaCuentaBancaria() {
+        return listaCuentaBancaria;
+    }
+
+    public void setListaCuentaBancaria(ArrayList<CuentaBancaria> listaCuentaBancaria) {
+        this.listaCuentaBancaria = listaCuentaBancaria;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public static long getTotalcliente() {
+        return totalcliente;
+    }
+
+    public static void setTotalcliente(long totalcliente) {
+        Cliente.totalcliente = totalcliente;
+    }
+    
+    
+    
+    
     void createCliente(String nombre, String cedula) {
         nombreCliente = nombre;
         cedulaCliente = cedula;
@@ -26,5 +69,23 @@ public class Cliente {
 
     void imprimirDatosCliente() {
         System.out.println("#" + idCliente + " C.I. " + cedulaCliente + "   " + nombreCliente);
+    }
+
+    boolean buscarClientebyCedula(String cedula) {
+        if (cedula.equals(cedulaCliente)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void addCuentaBancaria(CuentaBancaria cuenta) {
+        listaCuentaBancaria.add(cuenta);
+    }
+
+    public void imprimirCuentasBancaria(){
+        for (int i = 0; i < listaCuentaBancaria.size(); i++) {
+            System.out.println("#"+listaCuentaBancaria.get(i).getNumeroCuenta()+" - "+listaCuentaBancaria.get(i).getTipoCuenta().getDescripcion()+"    $"+listaCuentaBancaria.get(i).getSaldo());
+            
+        }
     }
 }
